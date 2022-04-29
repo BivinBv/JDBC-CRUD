fd9b0696ee18e0f9a69cc3c4309fddcbad2e6e06
BivinBv <bivin.b@techversantinfo.com>
2022-04-29 17:03:22 +0530

    view operation

--- Crud.java
+++ Crud.java
@@ -12,11 +12,30 @@ class JDBC
 		con=DriverManager.getConnection(url,uname,pass);
 		return con;
 	}
+	void view()
+	{
+		try
+		{
+		String query="select * from employee";
+		Statement st=getconnection().createStatement();
+		ResultSet rs=st.executeQuery(query);
+		while(rs.next())
+		{
+			int empid=rs.getInt("Employee_id");
+			String empname=rs.getString("Employee_name");
+			int managerid=rs.getInt("manager_id");
+			String projectid=rs.getString("project_id");
+			System.out.println(empid+"  "+empname+"  "+managerid+"  "+projectid);
+		}
+		}catch(Exception e){System.out.println(e);}
+	}
+	
 }
 public class Crud {
 
 	public static void main(String[] args) {
-		
+		JDBC j=new JDBC();
+		j.view();
 
 	}
 
