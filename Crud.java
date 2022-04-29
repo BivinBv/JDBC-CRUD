d725f2ca371f145c7642cd7f7cc95fc4f16494be
BivinBv <bivin.b@techversantinfo.com>
2022-04-29 16:51:58 +0530

    Connection established

--- Crud.java
+++ Crud.java
@@ -1,5 +1,18 @@
 package jdbccrud;
 import java.sql.*;
+class JDBC
+{
+	public static Connection getconnection() throws Exception 
+	{
+		Connection con=null;
+		String url="jdbc:mysql://localhost:3306/companydlts";
+		String uname="root";
+		String pass="root";
+		Class.forName("com.mysql.jdbc.Driver");
+		con=DriverManager.getConnection(url,uname,pass);
+		return con;
+	}
+}
 public class Crud {
 
 	public static void main(String[] args) {
